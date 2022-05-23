d239c45b018690a898279f534080f10bfebe1b80
teresapanmei <95527003+teresapanmei@users.noreply.github.com>
2022-05-23 11:49:41 +0530

    Delete alphavet.py

--- alphavet.py
+++
@@ -1,7 +0,0 @@
-user=(input("enter character"))
-if user>="a" and user<="z" or user>="A" and user<="Z":
-    print("alphabet")
-elif user>="0":
-    print("digit")
-else:
-    print("special character")
